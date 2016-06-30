5a11e4366213d5fa4393b2ebc2552e07bb47df80
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-30 16:10:00 -0700

    adding winning functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,8 @@
 function Player(id, sym) {
   this.id = id;
   this.symbol = sym;
+  this.numbers = {};
+  this.letters = {};
 }
 
 function TicTacToe() {
@@ -17,11 +19,52 @@ TicTacToe.prototype = {
   play: function(player, button) {
     if (button.text() !== '') {return}
     button.text(player.symbol);
+    var letter = button.data('cell')[0];
+    var num = button.data('cell')[1];
 
-    if (player.id === 1) {
+    this._inc_or_create_key(player.numbers, num);
+    this._inc_or_create_key(player.letters, letter);
+
+    if (this.won(player)) {
+      console.log('PLAYER ' + player.id + " WINS!");
+    } else if (player.id === 1) {
       this.turn = this.player_two;
     } else {
       this.turn = this.player_one;
+    }
+  },
+
+  won: function(player) {
+    // if letters are all the same, numbers are all the same, or letters & numbers different
+    // then the player wins (letters/numbers from data-cell attr in buttons)
+    var winner = false;
+    var num_keys = Object.keys(player.numbers);
+    var letter_keys = Object.keys(player.letters);
+
+    if (num_keys.length === 3 && letter_keys.length === 3) {
+      return true;
+    }
+
+    for (var n_key of num_keys) {
+      if (player.numbers[n_key] === 3) {
+        winner = true;
+      } 
+    }
+
+    for (var l_key of letter_keys) {
+      if (player.letters[l_key] === 3) {
+        winner = true;
+      }
+    }
+
+    return winner;
+  }, 
+
+  _inc_or_create_key: function(obj, key) {
+    if (obj[key]) {
+      obj[key]++;
+    } else {
+      obj[key] = 1;
     }
   }
 }
@@ -38,4 +81,3 @@ $(document).on('ready', function() {
     ttt.play(ttt.turn, button);
   })
 })
-    
