0dfaa0dea928b92d25564fd93eb2a3847dab7ab9
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-30 13:52:29 -0700

    created a Player class which keeps track of player id and symbol. turns alternate when a button is clicked. the player's symbol shows on the square that they clicked.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,34 @@
+function Player(id, sym) {
+  this.id = id;
+  this.symbol = sym;
+}
+
 function TicTacToe() {
-  
+  this.player_one = new Player(1, '🌶');
+  this.player_two = new Player(2, '🍹');
+  this.turn = this.player_one;
 }
 
 TicTacToe.prototype = {
   show: function() {
     $("#tic-tac-toe").show();
     $('.play').hide();
+  },
+  play: function(player, button) {
+    console.log(player);
+    var selected_button = button.data('cell');
+    var sym;
+    player.id === 1 ? sym = '🌶' : sym = '🍹';
+    button.text(sym);
+    console.log(button);
+    if (player.id === 1) {
+      this.turn = this.player_two;
+    } else {
+      this.turn = this.player_one;
+    }
   }
-
 }
+
+$(document).on('ready', function() {
+
+})
