75aa2187d4451601346d8836eaf1b458d69f39c4
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-01 13:02:58 -0700

    if the game is over, no more turns can be played until the board is reset using the reset button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,11 +12,12 @@ function TicTacToe() {
   this.player_two = new Player(2, '🍹');
   this.turn = this.player_one;
   this.turn_counter = 0;
+  this.game_over = false;
 }
 
 TicTacToe.prototype = {
   play: function(player, button) {
-    if (button.text() !== '') {return}
+    if (button.text() !== '' || this.game_over === true) {return false}
     button.text(player.symbol);
     var square = button.data('cell');
     var letter = square[0];
@@ -31,6 +32,7 @@ TicTacToe.prototype = {
     inc_or_create_key(player.numbers, num);
     inc_or_create_key(player.letters, letter);
     this.turn_counter++;
+    return true;
   },
 
   switch_turns: function(player) {
@@ -49,6 +51,7 @@ TicTacToe.prototype = {
     // if any of the number counts is 3, they have a vertical win
     for (var n_key of num_keys) {
       if (player.numbers[n_key] === 3) {
+        this.game_over = true;
         return true;
       } 
     }
@@ -56,6 +59,7 @@ TicTacToe.prototype = {
     // if any of the letter counts is 3, they have a horizontal win
     for (var l_key of letter_keys) {
       if (player.letters[l_key] === 3) {
+        this.game_over = true;
         return true;
       }
     }
@@ -64,6 +68,7 @@ TicTacToe.prototype = {
     // opposite corners, they have a diagonal win
     if (num_keys.length === 3 && letter_keys.length === 3 && player.center_sq === true) {
       if (containsAll(['A1', 'C3'], player.corners) || containsAll(['A3', 'C1'], player.corners)) {
+        this.game_over = true;
         return true;
       }
     }
@@ -73,10 +78,10 @@ TicTacToe.prototype = {
 
   check_winner: function(player) {
     if (this.won(player)) {
-      $('p').text('PLAYER ' + player.id + ' WINS!');
+      $('.outcome').text(player.symbol + ' WINS!');
       // $('.reset').css("display", "block");
     } else if (this.turn_counter === 9) {
-      $('p').text("IT'S A DRAW!");
+      $('.outcome').text("IT'S A DRAW!");
       $('p').addClass('draw');
     }
   }
@@ -103,9 +108,11 @@ $(document).on('ready', function() {
   $('.grid').on('click', function(event) {
     event.preventDefault();
     var button = $(this);
-    ttt.play(ttt.turn, button);
-    ttt.check_winner(ttt.turn);
-    ttt.switch_turns(ttt.turn);
+    var played = ttt.play(ttt.turn, button);
+    if (played) {
+      ttt.check_winner(ttt.turn);
+      ttt.switch_turns(ttt.turn);
+    }
   })
   $('.reset').on('click', function(event) {
     event.preventDefault();
